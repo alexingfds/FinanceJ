be4bec2775734d0ccaba683bdc09ccbec9d20cee
richardson <richardson.alexandre.1@ens.etsmtl.ca>
2019-10-31 19:46:10 -0400

    Ajouter gabarit des classes testes CategoryTest, LedgerTest,ReportsTest

---
+++ src/test/java/ca/etsmtl/log240/financej/CategoryTest.java
@@ -0,0 +1,9 @@
+package ca.etsmtl.log240.financej;
+import org.uispec4j.Table;
+import org.uispec4j.Trigger;
+import org.uispec4j.Window;
+import org.uispec4j.interception.WindowHandler;
+import org.uispec4j.interception.WindowInterceptor;
+
+public class CategoryTest extends  FinancejAbstractTest{
+}
