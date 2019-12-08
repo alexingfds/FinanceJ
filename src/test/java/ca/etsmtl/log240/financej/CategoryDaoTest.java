a236b136d9130992e63d7904694595d15f9d5697
richardson <richardson.alexandre.1@ens.etsmtl.ca>
2019-12-07 23:53:57 -0500

    test AccountDaoTest tester la connexion

---
+++ src/test/java/ca/etsmtl/log240/financej/CategoryDaoTest.java
@@ -0,0 +1,16 @@
+package ca.etsmtl.log240.financej;
+import org.junit.Test;
+
+import java.sql.Connection;
+
+import static org.junit.Assert.assertNotNull;
+
+public class CategoryDaoTest {
+    @Test
+
+    public void  testConnection(){
+        Connection  conn =null;
+        conn =DerbyUtils.getInstance().getConnexion();
+        assertNotNull(conn);
+    }
+}
